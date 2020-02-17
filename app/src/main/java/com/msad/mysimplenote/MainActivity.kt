52b9f54d55920ada4533ef4c159f92434bc854a8
Muharrem Servet ANKARALI <msankarali@outlook.com>
2020-02-17 22:29:51 +0300

    font and all layout styles added

--- app/src/main/java/com/msad/mysimplenote/MainActivity.kt
+++ app/src/main/java/com/msad/mysimplenote/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.msad.mysimplenote
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val sharedPreferences = this.getSharedPreferences(packageName, Context.MODE_PRIVATE)
+        val a: String? = sharedPreferences.getString("note", "")
+        editText.setText(a)
+        supportActionBar?.hide()
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        val sharedPreferences = this.getSharedPreferences(packageName, Context.MODE_PRIVATE)
+        sharedPreferences.edit().putString("note", editText.text.toString()).apply()
     }
 }
